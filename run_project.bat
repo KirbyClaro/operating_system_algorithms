4837d0d1b0740084b7d2189ed73167d2a02cc2c9
Kirby Claro <Kirbychristianclaro@gmail.com>
2026-06-27 05:49:11 +0800

    added bat to run project

---
+++ run_project.bat
@@ -0,0 +1,8 @@
+@echo off
+title OS Algorithms Dashboard Launcher
+echo ===================================================
+echo Starting Operating System Algorithms Dashboard...
+echo Please do not close this black window.
+echo ===================================================
+python main_menu.py
+pause
